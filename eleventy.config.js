50e3b0cba46860ffd6f110105682787341202121
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2025-01-12 23:21:35 +0000

    CNAME - move this into the _src directory
    ... and add the passthrough config to allow it to be copied.

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,7 @@ import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 export default function (config) {
   config.setInputDirectory("_src");
   config.setLayoutsDirectory("_layouts");
+  config.addPassthroughCopy("_src/CNAME");
   config.setLiquidOptions({
     outputEscape: "escape",
     strictVariables: true,
