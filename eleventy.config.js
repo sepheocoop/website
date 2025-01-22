354bb348a3fe56d084dc2ceb5a88e0be9b50981b
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2025-01-22 20:09:17 +0000

    eleventy.config.js - add passthrough for .css files

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,6 +4,7 @@ export default function (config) {
   config.setInputDirectory("_src");
   config.setLayoutsDirectory("_layouts");
   config.addPassthroughCopy("_src/CNAME");
+  config.addPassthroughCopy("_src/**/*.css");
   config.setLiquidOptions({
     outputEscape: "escape",
     strictVariables: true,
