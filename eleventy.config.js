23b2cb2aae018be85f35819c462a53ad57369fc3
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2025-01-22 21:07:00 +0000

    eleventy.config.js,.editorconfig - adapt for less
    .editorconfig - Add .less to 2-space indented files
    eleventy.config.js - Add preprocessor extension

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,8 @@
 import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
+import less from "less";
 
 export default function (config) {
+  config.addTemplateFormats("less");
   config.setInputDirectory("_src");
   config.setLayoutsDirectory("_layouts");
   config.addPassthroughCopy("_src/CNAME");
@@ -13,4 +15,22 @@ export default function (config) {
     jsTruthy: true,
   });
   config.addPlugin(EleventyHtmlBasePlugin);
+
+  // Transform less.js
+  // Adapted from https://www.11ty.dev/docs/languages/custom/#example-add-sass-support-to-eleventy
+  config.addExtension("less", {
+    outputFileExtension: "css",
+    useLayouts: false,
+    compile: async (inputContent) => {
+      // Hints from https://stackoverflow.com/a/55515382
+      const result = await less.render(inputContent, {
+        strict: "parens-division",
+      });
+
+      // This is the render function, `data` is the full data cascade
+      return async (data) => {
+        return result.css;
+      };
+    },
+  });
 }
